2576096d6a8f02018360a355af8052df278c618a
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-18 18:57:42 -0400

    Lists now properly represent their data type

--- src/scratchSchemas/scratchListSchema.ts
+++ src/scratchSchemas/scratchListSchema.ts
@@ -1,8 +1,5 @@
 import z from "zod";
 
-export const ScratchListSchema = z.object({
-	name: z.string(),
-	contents: z.array(z.union([z.string(), z.number()])),
-});
+export const ScratchListSchema = z.array(z.union([z.string(), z.array(z.union([z.string(), z.number()]))]));
 
 export type ScratchList = z.infer<typeof ScratchListSchema>;
