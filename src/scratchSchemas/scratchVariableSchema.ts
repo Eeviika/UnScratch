e52d70aad316c1351da457a7b2e3620519378d8c
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-18 19:01:31 -0400

    variables now support booleans

--- src/scratchSchemas/scratchVariableSchema.ts
+++ src/scratchSchemas/scratchVariableSchema.ts
@@ -1,5 +1,5 @@
 import z from "zod";
 
-export const ScratchVariableSchema = z.array(z.union([z.number(), z.string()]));
+export const ScratchVariableSchema = z.array(z.union([z.number(), z.string(), z.boolean()]));
 
 export type ScratchVariable = z.infer<typeof ScratchVariableSchema>;
