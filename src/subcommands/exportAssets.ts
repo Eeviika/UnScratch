e6e9f6d07eae46672e34e707454e5300cce0d748
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-24 11:55:42 -0400

    Add exportAssets and exportGlobals functions for asset management

---
+++ src/subcommands/exportAssets.ts
@@ -0,0 +1,56 @@
+import { Logger } from "../classes/logger";
+import fs from "fs";
+import path from "path";
+import AdmZip from "adm-zip";
+import { endsWithAny } from "../lib/utils/stringUtils";
+
+
+export function exportAssets(projectPath: string, imageDir: string, soundDir: string, logger: Logger) {
+  // Unzip the current project.
+  const projectZip = new AdmZip(projectPath);
+  const zipEntries = projectZip.getEntries();
+
+  // Directly export sounds and images.
+
+  logger.verbose("Directly exporting sounds & images...");
+  zipEntries.forEach((zipEntry) => {
+    logger.verbose(`Exporting (${zipEntry.entryName})...`);
+    if (zipEntry.isDirectory) {
+      logger.verbose("It was a directory, so it was not exported.");
+      return;
+    }
+    if (
+      endsWithAny(zipEntry.entryName, [
+        ".png",
+        ".svg",
+        ".bmp",
+        ".jpeg",
+        ".webp",
+      ])
+    ) {
+      // Trying to cover my bases here
+      logger.verbose("Exporting to images directory...");
+      const buffer = zipEntry.getData();
+      const imageOutPath = path.join(
+        imageDir,
+        path.basename(zipEntry.entryName)
+      );
+      fs.writeFileSync(imageOutPath, buffer);
+      logger.verbose("Done!");
+      return;
+    }
+
+    if (endsWithAny(zipEntry.entryName, [".mp3", ".wav", ".ogg"])) {
+      // Same here
+      logger.verbose("Exporting to sounds directory...");
+      const buffer = zipEntry.getData();
+      const soundOutPath = path.join(
+        soundDir,
+        path.basename(zipEntry.entryName)
+      );
+      fs.writeFileSync(soundOutPath, buffer);
+      logger.verbose("Done!");
+      return;
+    }
+  });
+}
