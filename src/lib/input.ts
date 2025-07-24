4fd621280be7326006ea492e26304ddb6304fb79
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-24 11:55:55 -0400

    Add askYesNo function for user input confirmation

---
+++ src/lib/input.ts
@@ -0,0 +1,25 @@
+import readline from "readline";
+
+export function askYesNo(
+	prompt: string,
+	yesDefault: boolean = false
+): Promise<boolean> {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+    if (yesDefault) {
+        prompt += " [Y/n] ";
+    } else {
+        prompt += " [y/N] ";
+    }
+
+	return new Promise((resolve) => {
+		rl.question(prompt, (answer) => {                                                                               
+			rl.close();
+			const val = answer.trim().toLowerCase();
+			resolve((val === "" && yesDefault) || val === "y" || val === "yes");
+		});
+	});
+}
