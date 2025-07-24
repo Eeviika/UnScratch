8d057286889f34aac34cf800d74eaa3d6f9343a3
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-24 11:56:15 -0400

    Refactor exportAll function to streamline project extraction process and remove unused code

--- src/commands/export_all.ts
+++ src/commands/export_all.ts
@@ -1,35 +1,14 @@
-import AdmZip from "adm-zip";
 import fs from "fs";
 import path from "path";
 import { exit } from "process";
-import readline from "readline";
 import { createTargetDir, validateProjectPath } from "../lib/cliValidator";
 import { Logger } from "../classes/logger";
-import { makeDirs, readScratchProject } from "../lib/fsUtils";
-
-// const DUMMY_AGENT_STRING =
-// 	"Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0";
-
-function askYesNo(
-	prompt: string,
-	yesDefault: boolean = false
-): Promise<boolean> {
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	return new Promise((resolve) => {
-		rl.question(prompt, (answer) => {
-			rl.close();
-			const val = answer.trim().toLowerCase();
-			resolve((val === "" && yesDefault) || val === "y" || val === "yes");
-		});
-	});
-}
-
-function endsWithAny(checkString: string, endsWith: string[]): boolean {
-	return endsWith.some((ext) => checkString.toLowerCase().endsWith(ext));
-}
+import { readScratchProject } from "../lib/utils/fsUtils";
+import { exportGlobals } from "../subcommands/exportGlobals";
+import { createProjectDirs } from "../subcommands/createProjectDirs";
+import { exportAssets } from "../subcommands/exportAssets";
+import { getProjectBufferData } from "../lib/getProjectBufferData";
+import { askYesNo } from "../lib/input";
 
 export async function exportAll(
 	projectPath: string,
@@ -50,89 +29,23 @@ export async function exportAll(
 	);
 
 	await (async () => {
-		logger.log(
-			`Extracting "${projectPath}" to directory "${targetDir}". Is this okay? ([Y]/N)`
-		);
-		const ok = await askYesNo("> ", true);
+		const ok = await askYesNo(`Extracting "${projectPath}" to directory "${targetDir}". Is this okay? ([Y]/N)\n> `, true);
 		if (!ok) {
 			logger.log("Operation cancelled.");
 			exit(0);
 		}
+		logger.log("Continuing with extraction...");
 	})();
 
-	// Unzip the current project.
-	const projectZip = new AdmZip(projectPath);
-	const zipEntries = projectZip.getEntries();
+	const { imageDir, soundDir, dataDir } =
+		createProjectDirs(targetDir, logger);
 
-	// Create a directory with the project name from targetDir.
-
-	logger.verbose(`Attempting to create (${targetDir})...`);
-	fs.mkdirSync(targetDir, { recursive: true });
-	logger.log("Created root directory.");
-
-	logger.verbose("Creating content directories...");
-	const imageDir = path.join(targetDir, "assets", "images");
-	const soundDir = path.join(targetDir, "assets", "sounds");
-	const spriteDir = path.join(targetDir, "sprites");
-	const dataDir = path.join(targetDir, "data");
-	const codeDir = path.join(targetDir, "code");
-
-	makeDirs([imageDir, soundDir, spriteDir, dataDir, codeDir]);
-	logger.log("Created content directories.");
-
-	let projectBufferData: Buffer<ArrayBufferLike> | undefined;
-
-	// Directly export sounds and images.
-
-	logger.verbose("Directly exporting sounds & images...");
-	zipEntries.forEach((zipEntry) => {
-		logger.verbose(`Exporting (${zipEntry.entryName})...`);
-		if (zipEntry.isDirectory) {
-			logger.verbose("It was a directory, so it was not exported.");
-			return;
-		}
-
-		if (zipEntry.entryName == "project.json") {
-			// TODO: Actually process project.json
-			projectBufferData = zipEntry.getData();
-			return;
-		}
-		if (
-			endsWithAny(zipEntry.entryName, [
-				".png",
-				".svg",
-				".bmp",
-				".jpeg",
-				".webp",
-			])
-		) {
-			// Trying to cover my bases here
-			logger.verbose("Exporting to images directory...");
-			const buffer = zipEntry.getData();
-			const imageOutPath = path.join(
-				imageDir,
-				path.basename(zipEntry.entryName)
-			);
-			fs.writeFileSync(imageOutPath, buffer);
-			logger.verbose("Done!");
-			return;
-		}
-
-		if (endsWithAny(zipEntry.entryName, [".mp3", ".wav", ".ogg"])) {
-			// Same here
-			logger.verbose("Exporting to sounds directory...");
-			const buffer = zipEntry.getData();
-			const soundOutPath = path.join(
-				soundDir,
-				path.basename(zipEntry.entryName)
-			);
-			fs.writeFileSync(soundOutPath, buffer);
-			logger.verbose("Done!");
-			return;
-		}
-	});
+	exportAssets(projectPath, imageDir, soundDir, logger);
 
 	logger.log("Extracted images / sounds.");
+
+	logger.verbose("Reading project.json from project...");
+	const projectBufferData = getProjectBufferData(projectPath);
 
 	if (projectBufferData == undefined) {
 		throw new Error(`Did not find "project.json" within project!`);
@@ -143,77 +56,7 @@ export async function exportAll(
 	);
 	const projectJsonData = readScratchProject(projectBufferData);
 
-	logger.verbose("Removing agent from metadata...");
-
-	projectJsonData.meta.agent = "";
-
-	logger.verbose("Looking for Stage (globals) sprite...");
-
-	const stage = projectJsonData.targets.find(
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		(target: any) => target.isStage === true
-	);
-
-	if (!stage) {
-		throw new Error("Could not find the Stage!");
-	}
-
-	logger.verbose("Found globals! Exporting global vars...");
-
-	const globalVariables: Record<
-		string,
-		Record<string, string | number | boolean>
-	> = {};
-
-	Object.entries(stage.variables).forEach((v) => {
-		globalVariables[v[0]] = {
-			name: v[1][0],
-			value: v[1][1],
-		};
-	});
-
-	const globalVariablesJSON = JSON.stringify(globalVariables, null, 2);
-	logger.verbose("Got global variables.");
-	logger.verbose("Exporting global lists...");
-
-	const globalLists: Record<
-		string,
-		{
-			name: string;
-			contents: Array<string | number | boolean>;
-		}
-	> = {};
-
-	Object.entries(stage.lists).forEach((v) => {
-		if (!Array.isArray(v[1][1])) {
-			throw new Error("Lists must have an ARRAY in index 1.");
-		}
-		globalLists[v[0]] = {
-			name: v[1][0].toString(),
-			contents: v[1][1],
-		};
-	});
-
-	const globalListsJSON = JSON.stringify(globalLists, null, 2)
-	logger.verbose("Got global lists.")
-	logger.verbose("Exporting broadcasts...")
-
-	fs.writeFileSync(
-		path.join(targetDir, "project.json"),
-		JSON.stringify(projectJsonData.meta, null, 2)
-	);
-	fs.writeFileSync(
-		path.join(dataDir, "global_variables.json"),
-		globalVariablesJSON
-	);
-	fs.writeFileSync(
-		path.join(dataDir, "global_lists.json"),
-		globalListsJSON
-	);
-	fs.writeFileSync(
-		path.join(dataDir, "broadcasts.json"),
-		JSON.stringify(stage.broadcasts, null, 2)
-	);
+	exportGlobals(projectJsonData, targetDir, dataDir, logger)
 
 	logger.log("Done!");
 	logger.log(`Project extracted to "${targetDir}"`);
