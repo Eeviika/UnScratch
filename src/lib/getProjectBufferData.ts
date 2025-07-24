dadc11f782b91d1be8a264225898b5fbcd2f90e5
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-07-24 11:56:07 -0400

    Add getProjectBufferData function to extract project.json from zip files

---
+++ src/lib/getProjectBufferData.ts
@@ -0,0 +1,13 @@
+import AdmZip from "adm-zip";
+export function getProjectBufferData(projectPath: string): Buffer | undefined {
+    const zip = new AdmZip(projectPath);
+    const zipEntries = zip.getEntries();
+
+    for (const entry of zipEntries) {
+        if (entry.entryName === "project.json") {
+            return entry.getData();
+        }
+    }
+
+    return undefined;
+}
