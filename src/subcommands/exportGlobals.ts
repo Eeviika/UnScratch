82e1c82b0cd26712cb8607546baa009c23b4d895
Eeviika <84326942+Eeviika@users.noreply.github.com>
2025-09-21 21:27:38 -0400

    Modified scripts to use dataUtils

--- src/subcommands/exportGlobals.ts
+++ src/subcommands/exportGlobals.ts
@@ -2,6 +2,7 @@ import { Logger } from "../classes/logger";
 import { ScratchProject } from "../scratchSchemas/scratchProjectSchema";
 import fs from "fs";
 import path from "path";
+import { JSONParseLists, JSONParseVariables } from "../lib/utils/dataUtils";
 
 const DUMMY_AGENT_STRING =
 	"Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0";
@@ -27,44 +28,7 @@ export function exportGlobals(
 		throw new Error("Could not find the Stage!");
 	}
 
-	logger.verbose("Found globals! Finding global vars...");
-
-	const globalVariables: Record<
-		string,
-		Record<string, string | number | boolean>
-	> = {};
-
-	Object.entries(stage.variables).forEach((v) => {
-		globalVariables[v[0]] = {
-			name: v[1][0],
-			value: v[1][1],
-		};
-	});
-
-	const globalVariablesJSON = JSON.stringify(globalVariables, null, 2);
-	logger.verbose("Got global variables.");
-	logger.verbose("Finding global lists...");
-
-	const globalLists: Record<
-		string,
-		{
-			name: string;
-			contents: Array<string | number | boolean>;
-		}
-	> = {};
-
-	Object.entries(stage.lists).forEach((v) => {
-		if (!Array.isArray(v[1][1])) {
-			throw new Error("Lists must have an Array[Variant] in index 1.");
-		}
-		globalLists[v[0]] = {
-			name: v[1][0].toString(),
-			contents: v[1][1],
-		};
-	});
-
-	const globalListsJSON = JSON.stringify(globalLists, null, 2);
-	logger.verbose("Got global lists.");
+	logger.verbose("Found globals!");
 
 	logger.verbose("Exporting project metadata...");
 	fs.writeFileSync(
@@ -75,10 +39,13 @@ export function exportGlobals(
 	logger.verbose("Exporting global variables and lists...");
 	fs.writeFileSync(
 		path.join(dataDir, "global_variables.json"),
-		globalVariablesJSON,
+		JSONParseVariables(stage),
 	);
 
-	fs.writeFileSync(path.join(dataDir, "global_lists.json"), globalListsJSON);
+	fs.writeFileSync(
+		path.join(dataDir, "global_lists.json"),
+		JSONParseLists(stage),
+	);
 
 	logger.verbose("Exporting broadcasts...");
 	fs.writeFileSync(
--- src/subcommands/exportSprite.ts
+++ src/subcommands/exportSprite.ts
@@ -2,50 +2,68 @@ import path from "path";
 import { Logger } from "../classes/logger";
 import { ScratchSprite } from "../scratchSchemas/scratchSpriteSchema";
 import fs from "fs";
+import { JSONParseLists, JSONParseVariables } from "../lib/utils/dataUtils";
 
-export function exportSprite (
-    sprite: ScratchSprite,
-    dataDir: string,
-    spriteDir: string,
-    codeDir: string,
-    logger: Logger
+export function exportSprite(
+	sprite: ScratchSprite,
+	dataDir: string,
+	spriteDir: string,
+	codeDir: string,
+	logger: Logger,
 ) {
-    logger.verbose(`Exporting sprite "${sprite.name}"...`);
+	logger.verbose(`Exporting sprite "${sprite.name}"...`);
 
-    if (sprite.isStage) {
-        logger.warn("Cannot export a Stage as a sprite (use exportGlobals instead). Skipping export.");
-        return;
-    }
-    
-    const newSpriteData = {
-        name: sprite.name,
-        x: sprite.x ?? 0, // Default to 0 if x is undefined
-        y: sprite.y ?? 0, // Default to 0 if y is undefined
-        size: sprite.size ?? 100,
-        direction: sprite.direction ?? 90,
-        visible: sprite.visible ?? true,
-        layerOrder: sprite.layerOrder ?? 0,
-        costumes: sprite.costumes,
-        sounds: sprite.sounds,
-        draggable: sprite.draggable ?? false,
-        volume: sprite.volume ?? 100,
-        tempo: sprite.tempo ?? 60,
-        videoTransparency: sprite.videoTransparency ?? 0,
-        textToSpeechLanguage: sprite.textToSpeechLanguage ?? null,
-        videoState: sprite.videoState ?? "off",
-        rotationStyle: sprite.rotationStyle ?? "normal",
-        // blocks: sprite.blocks, will be exported to codeDir
-        // comments: sprite.comments, will be exported to codeDir
-        // variables: sprite.variables, will be exported to dataDir
-        // lists: sprite.lists, will be exported to dataDir
-        // broadcasts: sprite.broadcasts, literally not used in Scratch 3.0 unless the sprite is a Stage
-        currentCostume: sprite.currentCostume,
-    }
+	if (sprite.isStage) {
+		logger.warn(
+			"Cannot export a Stage as a sprite (use exportGlobals instead). Skipping export.",
+		);
+		return;
+	}
 
-    const spriteFilePath = path.join(spriteDir, `${sprite.name}.json`);
-    logger.verbose(`Writing sprite data to ${spriteFilePath}...`);
-    const spriteData = JSON.stringify(newSpriteData, null, 2);
-    fs.writeFileSync(spriteFilePath, spriteData);
+	const newSpriteData = {
+		name: sprite.name,
+		x: sprite.x ?? 0,
+		y: sprite.y ?? 0,
+		size: sprite.size ?? 100,
+		direction: sprite.direction ?? 90,
+		visible: sprite.visible ?? true,
+		layerOrder: sprite.layerOrder ?? 0,
+		costumes: sprite.costumes,
+		sounds: sprite.sounds,
+		draggable: sprite.draggable ?? false,
+		volume: sprite.volume ?? 100,
+		tempo: sprite.tempo ?? 60,
+		videoTransparency: sprite.videoTransparency ?? 0,
+		textToSpeechLanguage: sprite.textToSpeechLanguage ?? null,
+		videoState: sprite.videoState ?? "off",
+		rotationStyle: sprite.rotationStyle ?? "normal",
+		// blocks: sprite.blocks, will be exported to codeDir
+		// comments: sprite.comments, will be exported to codeDir
+		// variables: sprite.variables, will be exported to dataDir
+		// lists: sprite.lists, will be exported to dataDir
+		// broadcasts: sprite.broadcasts, literally not used in Scratch 3.0 unless the sprite is a Stage
+		currentCostume: sprite.currentCostume,
+	};
 
+	const spriteFilePath = path.join(spriteDir, `${sprite.name}.json`);
+	logger.verbose(`Writing sprite data to ${spriteFilePath}...`);
+	const spriteData = JSON.stringify(newSpriteData, null, 2);
+	fs.writeFileSync(spriteFilePath, spriteData);
 
+	const codeFilePath = path.join(codeDir, `${sprite.name}_code.json`);
+	logger.verbose(`Writing sprite code to ${codeFilePath}...`);
+	const spriteCodeData = JSON.stringify(sprite.blocks, null, 2);
+	fs.writeFileSync(spriteFilePath, spriteCodeData);
+
+	const spriteVariablesFilePath = path.join(
+		dataDir,
+		`${sprite.name}_variables.json`,
+	);
+	const spriteListsFilePath = path.join(
+		dataDir,
+		`${sprite.name}_lists.json`
+	)
+
+	fs.writeFileSync(spriteVariablesFilePath, JSONParseVariables(sprite))
+	fs.writeFileSync(spriteListsFilePath, JSONParseLists(sprite))
 }
